833c5f06c94a4efb30ffd5a92db4fb2324ece3da
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-09-21 15:49:40 +0200

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -11,34 +11,69 @@ from selenium.webdriver.common.by import By
 class LikeeDownloader:
 
     def __init__(self, args):
+        self.program_version_number = "2022.1.0.0"
         self.option = webdriver.FirefoxOptions()
         self.option.add_argument('--headless')
         self.driver = webdriver.Firefox(options=self.option)
         self.user_profile_url = "https://likee.video/@{}"
         self.user_videos_api_endpoint = "https://api.like-video.com/likee-activity-flow-micro/videoApi/getUserVideo"
+        self.update_check_endpoint = "https://api.github.com/repos/rly0nheart/Likee-Downloader/releases/latest"
 
 
     def notice(self):
-        notice_msg = """
-    Likee video downloader
-    Copyright (C) 2022  Richard Mwewa
+        notice_msg = f"""
+    Likee-Downloader {self.program_version_number} Copyright (C) 2022  Richard Mwewa
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version."""
+    (at your option) any later version.
+    """
         return notice_msg
 
 
-    def get_updates(self):
-        print('updating...')
+    def check_and_get_updates(self):
+        print(self.notice())
+        response = requests.get(self.update_check_endpoint).json()
+        if response['tag_name'] == self.program_version_number:
+            """Ignore if the program is up to date"""
+            pass
+        else:
+            update_prompt = input(f"[?] A new release is available ({response['tag_name']}). Would you like to install it? (y/n) ")
+            if update_prompt.lower() == "y":
+                files_to_update = ['downloader.py', 'geckodriver.exe', 'README.md', 'requirements.txt']
+                for file in tqdm(files_to_update, desc=f'Updating'):
+                    data = requests.get(f'https://raw.githubusercontent.com/rly0nheart/Likee-Downloader/master/{file}')
+                    with open(file, "wb") as f:
+                        f.write(data.content)
+                        f.close()
+                print(f"Updated!: ")
+            else:
+                pass
 
 
     def capture_screenshot(self):
         print("\n[~] Capturing profile screenshot:", args.username)
         self.driver.get(self.user_profile_url.format(args.username))
         self.driver.get_screenshot_as_file(f"downloads/screenshots/{args.username}_likee-downloader.png")
-        print(f"[~] Captured: downloads/screenshots/{args.username}_likee-scraper.png")
+        print(f"[~] Captured: downloads/screenshots/{args.username}_likee-downloader.png")
+
+
+    def get_user_id(self):
+        print("\n[~] Obtaining userId (This may take a while)...", end='')
+        response = requests.get(f"{self.user_profile_url.format(args.username)}/video/{self.get_user_videoId()}")
+        regex_pattern = re.compile('window.data = ({.*?});', flags=re.DOTALL | re.MULTILINE)
+        str_data = regex_pattern.search(response.text).group(1)
+        json_data = json.loads(str_data)
+        payload = {"country": "US",
+                   "count": 100,
+                   "page": 1,
+                   "pageSize": 28,
+                   "tabType": 0,
+                   "uid": json_data['uid']
+                   }
+        print("\n[+] userId obtained:", json_data['uid'])
+        return payload
 
     
     def get_user_videoId(self):
@@ -55,54 +90,35 @@ class LikeeDownloader:
 
     
     def download_user_videos(self):
-        print(self.notice())
+        self.check_and_get_updates()
         if args.screenshot:
             self.capture_screenshot()
 
-        print("\n[~] Obtaining userId (This may take a while)...", end='')
-        response = requests.get(f"{self.user_profile_url.format(args.username)}/video/{self.get_user_videoId()}")
-        regex_pattern = re.compile('window.data = ({.*?});', flags=re.DOTALL | re.MULTILINE)
-        str_data = regex_pattern.search(response.text).group(1)
-        json_data = json.loads(str_data)
-        payload = {"country": "US",
-                   "count": 100,
-                   "page": 1,
-                   "pageSize": 28,
-                   "tabType": 0,
-                   "uid": json_data['uid']
-                   }
-        print("\n[+] userId obtained:", json_data['uid'])
-        print("[~] Getting videos...", end='')
-        response = requests.post(self.user_videos_api_endpoint, json=payload).json()
+        response = requests.post(self.user_videos_api_endpoint, json=self.get_user_id()).json()
         videos = response['data']['videoList']
         print(f'\n[+] Found {len(videos)} videos\n')
         downloaded_videos = 0
         for video in videos[:10]:
-
             # Printing video information
             print(video['msgText'])
             for video_key, video_value in video.items():
                 print(f"├─ {video_key}: {video_value}")
-
             downloaded_videos += 1
             response = requests.get(video['videoUrl'], stream=True)
             with open(f"downloads/videos/{args.username}_{video['postId']}.mp4", "wb") as file:
-                for chunk in tqdm(response.iter_content(chunk_size=1024 * 1024),
-                                  desc=f"Downloading: {video['postId']}.mp4"):
+                for chunk in tqdm(response.iter_content(chunk_size=1024 * 1024), desc=f"Downloading: {video['postId']}.mp4"):
                     if chunk:
                         file.write(chunk)
 
             print(f"Downloaded: {file.name}\n")
-        print(f"[+] Complete: {downloaded_videos} videos were downloaded.")
+        print(f"[+] Complete: {downloaded_videos}/{len(videos)} videos were downloaded.")
 
 
 parser = argparse.ArgumentParser(description='Likee-Downloader — by Richard Mwewa ')
 parser.add_argument('username', help='specify target username')
-parser.add_argument('--videos', help='download videos (downloads 10 videos maximum)', action='store_true')
-parser.add_argument('--screenshot', help='capture screenshot of target\'s profile', action='store_true')
+parser.add_argument('-s', '--screenshot', help='capture a screenshot of the target\'s profile', action='store_true')
 parser.add_argument('-d', '--debug', help='enable debug mode', action='store_true')
-parser.add_argument('-u', '--update', help='update program', action='store_true')
-parser.add_argument('-v', '--version', version='1.0', action='version')
+parser.add_argument('-v', '--version', version='2022.1.0.0', action='version')
 args = parser.parse_args()
 
 if args.debug:
@@ -110,13 +126,10 @@ if args.debug:
 
 if __name__ == "__main__":
     try:
-        if args.update:
-            LikeeDownloader(args).get_updates()
-        else:
-            LikeeDownloader(args).download_user_videos()
+        LikeeDownloader(args).download_user_videos()
 
     except KeyboardInterrupt:
         print("[!] Process interrupted with Ctrl+C.")
 
     except Exception as e:
-        raise Exception(f"[x] An error occured: {e}")
+        print(f"[x] An error occured: {e}")
