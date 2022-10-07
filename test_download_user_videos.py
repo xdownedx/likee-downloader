94e0e02be6db9f4426adaf82bc5a54de674387b7
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-10-07 02:06:55 +0200

    Delete test_download_user_videos.py

--- test_download_user_videos.py
+++
@@ -1,5 +0,0 @@
-from downloader import download_user_videos
-     
-def test_download_user_videos():
-    username = 'Baselrajab'
-    download_user_videos(username, screenshot=None, videos_count=1)
