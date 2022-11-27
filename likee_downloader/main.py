e5346f2729e763034f1908cc11f60a64c80b6a0e
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-11-27 02:23:52 +0200

    Update main.py

--- likee_downloader/main.py
+++ likee_downloader/main.py
@@ -1,12 +1,13 @@
 from likee_downloader.downloader import LikeeDownloader
 
-def main():
+
+def downloader():
     try:
         start = LikeeDownloader()
         start.download_user_videos()
     
     except KeyboardInterrupt:
-        print('Process interrupted with Ctrl+C.')
+        print('[CTRLC] Process interrupted with Ctrl+C.')
 
     except Exception as e:
-        print('An error occured:', e)
+        print('[ERROR] An error occured:', e)
